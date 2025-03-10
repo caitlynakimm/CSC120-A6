6a81385f775a5c77536b178ddcce689952dabdd3
Caitlyn Kim <cakim@smith.edu>
2025-03-10 02:10:11 -0400

    added tests

--- TrainTest.java
+++ TrainTest.java
@@ -1,17 +1,34 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
 
 public class TrainTest {
+
+    public Engine myEngine;
+
+    @Before //prework before every test
+    public void setup() {
+        this.myEngine = new Engine(FuelType.ELECTRIC, 100., 100.);
+    }
 
     // Engine Tests
     @Test
     public void testEngineConstructor() {
-        fail();
+        //FuelType testFuelType = FuelType.ELECTRIC;
+        //double testCurrentLevel = 100;
+        //double testMaxFuelLevel = 100;
+        FuelType expectedFuelType = FuelType.ELECTRIC;
+        double expectedCurrentLevel = 100.;
+        double expectedMaxLevel = 100.;
+        assertEquals(expectedFuelType, myEngine.getFuelType());
+        assertEquals(expectedCurrentLevel, myEngine.getCurrentFuel(), .001);
+        assertEquals(expectedMaxLevel, myEngine.getMaxFuel(), .001);
     }
 
     @Test
     public void testEngineGo() {
-        fail();
+        boolean currentFuelChangeResult = true;
+        assertEquals(currentFuelChangeResult, myEngine.go());
     }
 
     // Car Tests
--- reflection.md
+++ reflection.md
@@ -2,3 +2,5 @@ Use this file to record your reflection on this assignment.
 
 What are your initial impressions of how `Unit Testing` affects your programming?
 What worked, what didn't, what advice would you give someone taking this course in the future?
+
+#in ur response include a detailed description of the tests you implemented and what they do**
